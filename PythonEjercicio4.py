8bf8ac8fa14da93d1cf80235709e5cd2c94db4c7
Mauri <71477454+Mauriljb@users.noreply.github.com>
2022-10-13 03:09:27 -0300

    Add files via upload

---
+++ PythonEjercicio4.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 13 02:57:59 2022
+
+@author: mauriljb
+"""
+print("Primero se verá la resolución con el bucle While")
+contador=100
+while contador!=0:
+    print(contador)
+    contador=contador-1
+    pass
+
+print("Ahora veremos la resolución del problema con el bucle For")
+for i in range(100):
+    print(100-i)
+    pass
